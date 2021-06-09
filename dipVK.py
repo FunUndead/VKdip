9f2c611d6034aeefa715cb03011a658b713f64a1
Maxim Shihatarov <shix97@mail.ru>
2021-06-10 00:32:04 +0400

    Finish

--- dipVK.py
+++ dipVK.py
@@ -1,7 +1,14 @@
 import requests
 import json
-import pprint
 from pprint import pprint
+from datetime import datetime
+import time
+from tqdm import tqdm
+import os
+
+
+
+
 
 class VKUser:
     url = 'https://api.vk.com/method/'
@@ -27,19 +34,57 @@ class VKUser:
         }
         res = requests.get(photo_url, params={**self.params, **photo_params})
         vk_dict = res.json()
-        vk_list = []
-        vk_foto =[]
-        vk_app = []
-        for x in vk_dict['response']['items']:  # Как перебрать эти данные
-            for y in x:
-                print(y)
+        data_json_list = []
+
+        for x in tqdm(vk_dict['response']['items']):
+            file_url = x['sizes'][-1]['url']
+            data_json = x['sizes'][-1]['type']
+            file_name = str(x['likes']['count'])
+            date = x['date']
+            ts = int(date)
+            ts_new = str(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d'))
+            api = requests.get(file_url)
+            file_name_new = file_name+"-likes-"+ts_new+".jpg"
+            data_json_w = {}
+            data_json_w["file_name"] = file_name_new
+            data_json_w["size"] = data_json
+            data_json_list.append(data_json_w)
+            with open ("images/%s" % file_name_new, "wb") as f:
+                f.write(api.content)
+
+        with open('data.json', 'w', encoding='utf-8') as file:          #Запись в JSON файл
+            json.dump(data_json_list, file)
+
+        return print("Скачивание фото выполнено!")
+
+    def upload(self, token_yandex):
+    #на яндекс диске нужно создать папку images
+        path = 'images/'
+        files_list = os.listdir(path)
+        data = os.path.join(path)
+        #print(files_list)
+
+        for z in tqdm(files_list):
+            headers = {'Content-Type': 'application/json', 'Authorization': token_yandex}
+            upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+            headers_2 = headers
+            params = {"path": data+z, "overwrite": "true"}
+            response_2 = requests.get(upload_url, headers=headers_2, params=params)
+            x = response_2.json()
+            href = x.get("href")
+            response_3 = requests.put(href, data=open(path+z, 'rb'))
+            response_3.raise_for_status()
+
+        return print("Фотографии загружены на диск!")
 
 
 
-        return (vk_dict['response']['items'])
 
 
-token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-vk_client = VKUser(token, '5.130')
-pprint(vk_client.get_photo())
+token_vk = '' #Токен ВК
+token_yandex = '' #Токен яндекс диска
+vk_client = VKUser(token_vk, '5.130')
+vk_client.get_photo()
+vk_client.upload(token_yandex)
+
 
